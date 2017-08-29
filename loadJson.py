f99ad25023131436e60e045c058910404bbcbd7a
awaddell77 <waddell.andrew@gmail.com>
2017-08-29 14:37:04 -0700

    Added loadJson

--- loadJson.py
+++ loadJson.py
@@ -4,3 +4,9 @@ def loadJson(pathToFile):
     with open(pathToFile, encoding = 'utf-8') as jsonFile:
         data = json.load(jsonFile)
     return data
+
+def dumpJson(pathToFile, data):
+    fName = pathToFile
+    with open(fName, 'w') as jsonFile:
+        jsonFile.write(json.dumps(data))
+    print("Updated {0}".format(fName))
