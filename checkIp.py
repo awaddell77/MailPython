afc88abd4e186e54f87c340c04c56ae381da113d
awaddell77 <waddell.andrew@gmail.com>
2017-08-29 15:46:24 -0700

    Added command line support

--- checkIp.py
+++ checkIp.py
@@ -1,5 +1,8 @@
-import subprocess
+import subprocess, sys
 
 def checkIp():
     res = subprocess.check_output('curl ipv4bot.whatismyipaddress.com', shell=True)
     return str(res)
+
+if __name__ == "__main__":
+    checkIp()
